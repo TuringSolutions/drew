6a031911ab66bb137e90400084d22a3c9664bae2
TuringSolutions <(none)>
2023-03-24 03:53:07 +0530

    Feature: added footer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Foot from './components/Foot.vue';
+</script>
 
 <template>
   <div class="flex flex-col items-stretch justify-start">
     <RouterView></RouterView>
   </div>
+  <footer>
+    <foot></foot>
+  </footer>
 </template>
 
 <style scoped></style>
---
+++ src/components/Foot.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="grid grid-cols-2 gap-4 xl:gap-16 md:grid-cols-4 xl:grid-cols-6 bg-black text-white p-8">
+        <div class="flex flex-col xl:col-start-2 gap-2">
+            <div class="uppercase underline font-bold">Expertise and Services</div>
+            <div>Data Science & AI</div>
+            <div>Machine Learning</div>
+            <div>AI Software Development</div>
+            <div>AI Mobile Apps Development</div>
+            <div>Natural Language Processing</div>
+            <div>Data Capture & OCR</div>
+        </div>
+        <div class="flex flex-col xl:col-start-3 gap-2">
+            <div class="uppercase underline font-bold" >Big Data Solutions</div>
+            <div>Big Data Development</div>
+            <div>Data Architecture Engineering</div>
+            <div>Data Warehouse Engineering</div>
+            <div>BI & Data Visualizations</div>
+            <div>Predictive Analysis</div>
+            <div>Cloud Services</div>
+        </div>
+        <div class="flex flex-col xl:col-start-4 gap-2">
+            <div class="uppercase underline font-bold">About Us</div>
+            <div>Success Stories</div>
+            <div>Blog</div>
+            <div>Careers</div>
+            <div>About</div>
+            <div>Write to Us</div>
+        </div>
+        <div class="flex flex-col xl:col-start-5 gap-2">
+            <div class="flex flex-row">
+
+            </div>
+            <div>
+                Address goes here
+            </div>
+            <div>
+                Contact Email and Phone number
+            </div>
+        </div>
+    </div>
+</template>
